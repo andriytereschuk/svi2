59e0e8e530d86b322ce7f3984206f7d328210f99
andriytereschuk <shatskyat@gmail.com>
2021-04-20 22:37:29 +0300

    initial commit

---
+++ components/Head.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <div class="top">
+      <div class="container">
+        <div class="row align-center justify-between">
+          <nuxt-link to="/">
+            <img class="logo" src="images/logo.png" alt="" />
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+    <nav></nav>
+  </header>
+</template>
+
+<style lang="scss">
+.top {
+  height: 60px;
+  background-color: #fff;
+}
+
+.logo {
+  height: 38px;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <Head />
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  font-family: Roboto, -apple-system, Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  line-height: 24px;
+  background-color: #eee;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.container {
+  width: 1200px;
+  margin: 0 auto;
+  height: 100%;
+}
+
+.row {
+  display: flex;
+  height: 100%;
+}
+
+.align-center {
+  align-items: center;
+}
+
+.justify-between {
+  justify-content: space-between;
+}
+</style>
