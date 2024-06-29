30e3ef857a37f805952afd395e9ae730152bb1cf
Andrii Tereshchuk <shatskyat@gmail.com>
2024-06-29 09:02:45 +0200

    update banners

--- components/Bunner.vue
+++ components/Bunner.vue
@@ -15,32 +15,32 @@ export default {
           info: 'Будинок на 8-10 чол',
           image:
             'https://a.storyblok.com/f/153450/1000x750/e0cf0ed31c/cottage-general_1_1.jpg',
-          price: '3500 грн/доба',
+          price: '4000 грн/доба',
           link: '/homes/cottage',
         },
         {
-          title: 'Двомісний з балконом',
+          title: 'Двомісні',
           info: '',
           image:
-            'https://a.storyblok.com/f/153450/1000x750/2c04ff48a3/main_photo.jpg',
-          price: '550 грн/доба',
-          link: '/rooms/90265d8e-b286-417f-bdbf-6f215d613fab',
+            'https://a.storyblok.com/f/153450/1000x750/1e5aba2ac8/second_floor_room_4_3.jpg',
+          price: 'від 500 грн/доба',
+          link: '/categories/double',
+        },
+        {
+          title: 'Два + один',
+          info: '',
+          image:
+            'https://a.storyblok.com/f/153450/1200x801/e1348baf70/2024-1.jpeg',
+          price: 'від 750 грн/доба',
+          link: '/categories/triple',
         },
         {
           title: 'Апартаменти на 4-6 чол',
           info: '',
           image:
             'https://a.storyblok.com/f/153450/1000x750/4b4551bc2c/room_3_1.jpg',
-          price: '2100 грн/доба',
+          price: '2400 грн/доба',
           link: '/rooms/a1a67b5f-1e69-4f7d-b716-cfbee7daf7aa',
-        },
-        {
-          title: 'Двомісний з власним санвузлом',
-          info: '',
-          image:
-            'https://a.storyblok.com/f/153450/1000x750/43d7eff6b3/room_1_1.jpg',
-          price: '700 грн/доба',
-          link: '/rooms/39d486f0-16b9-430b-8c5b-2e6341a9add9',
         },
       ],
     }
